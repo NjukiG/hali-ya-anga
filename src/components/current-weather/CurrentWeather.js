c4b75f4dee2c2dbb27ce9c4a609ec48205958e6b
George <gnjuki19@gmail.com>
2023-02-24 12:59:27 +0300

    Did styling for the current weather component

--- src/components/current-weather/CurrentWeather.js
+++ src/components/current-weather/CurrentWeather.js
@@ -1,16 +1,46 @@
-import React from 'react'
-import "./CurrentWeather.css"
+import React from "react";
+import "./CurrentWeather.css";
 
 function CurrentWeather() {
   return (
-    <div className='weather'>
-        <div className='top'>
-            <p className='city'>Nairobi</p>
-            <p className='weather-description'>Sunny</p>
+    <div className="weather">
+      <div className="top">
+        <div>
+          <p className="city">Nairobi</p>
+          <p className="weather-description">Sunny</p>
         </div>
-        <img className='weather-icon' src='' alt='weather' />
+
+        <img className="weather-icon" src="icons/01d.png" alt="weather" />
+      </div>
+      <div className="bottom">
+        <p className="temperature">18°C</p>
+        <div className="details">
+          <div className="parameter-row">
+            <span className="parameter-label top">Details</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Feels like</span>
+            <span className="parameter-value">20°C</span>
+          </div>
+
+          <div className="parameter-row">
+            <span className="parameter-label">Wind speed</span>
+            <span className="parameter-value">2 km/h</span>
+          </div>
+
+          <div className="parameter-row">
+            <span className="parameter-label">Humidity</span>
+            <span className="parameter-value">23%</span>
+          </div>
+
+          <div className="parameter-row">
+            <span className="parameter-label">Pressure</span>
+            <span className="parameter-value">23 Pa</span>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default CurrentWeather
+export default CurrentWeather;
